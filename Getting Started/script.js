21c43403e5b3ebd857520e3985b04bd5506ba5a0
Grigorescu <36515413+GrigorescuOffice@users.noreply.github.com>
2019-02-20 00:07:36 +0200

    Add files via upload

---
+++ Getting Started/script.js
@@ -0,0 +1,14 @@
+function payment() {
+    document.getElementById("payment").style.display= "block" ;
+    document.getElementById("container").style.display="none" ; 
+}
+function end_payment(){
+    document.getElementById("payment").style.display= "none" ;
+    document.getElementById("container").style.display="block" ; 
+    
+    alert("Congrats !! You just made your girlfriend te happiest woman alive ! You won't regret this . ");
+}
+function cancel_payment(){
+    document.getElementById("payment").style.display= "none" ;
+    document.getElementById("container").style.display="block" ; 
+}
